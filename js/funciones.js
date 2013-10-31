1e9f15dbae32110e5381712cf1a37be56f809312
EPulgar <epulgar@maruri.ec>
2013-10-31 17:12:01 -0500

    kinetic move y drag

--- js/funciones.js
+++ js/funciones.js
@@ -157,4 +157,15 @@ ANIMACION BASICA
 -------------------------------------------------------------------------------
 */
 
-
+var canvas = $("#canvasElement");
+var context = canvas.get(0).getContext("2d");
+var canvasWidth = canvas.width();
+var canvasHeight = canvas.height();
+var x = 10;
+var y = 10;
+function moveBox() {
+context.clearRect(0,0, canvasWidth, canvasHeight);
+context.fillRect(x, y, 10, 10);
+x++;
+setTimeout(moveBox, 33);
+}
